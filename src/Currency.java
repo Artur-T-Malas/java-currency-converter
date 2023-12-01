5f8bde37a5677e62a4a4187cdc6e710604316e52
Artur T. Małas <artur.malas25@gmail.com>
2023-12-01 17:39:17 +0100

    Add classes Currency and CurrencyConverter

---
+++ src/Currency.java
@@ -0,0 +1,10 @@
+public class Currency {
+
+    String currencyName;
+    float usdExchangeRate;
+
+    public Currency(String currencyName, float usdExchangeRate) {
+        this.currencyName = currencyName;
+        this.usdExchangeRate = usdExchangeRate;
+    }
+}
--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -1,3 +1,6 @@
 public class CurrencyConverter {
-    
+    public static float convert(float amount, Currency currencyStart, Currency currencyFinal) {
+        float result = amount * currencyStart.usdExchangeRate / currencyFinal.usdExchangeRate;
+        return result;
+    }
 }
